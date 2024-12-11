a114e71c50a0231aee016faa57d8db7cf375a207
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2024-12-11 10:12:13 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -3,11 +3,10 @@ class Solution {
         int num = x;
         int sum = 0;
         for(int i= 1; i<=(int)(Math.log10(x)+1);i++){
-            sum = sum + num%10;
-            num = num / 10;
+            sum += num%10;
+            num /= 10;
         }
+        
         return (x % sum == 0)? true : false;
-        // return (x % (x/10 + x%10) == 0 ? true : false);
-        // return answer;
     }
 }

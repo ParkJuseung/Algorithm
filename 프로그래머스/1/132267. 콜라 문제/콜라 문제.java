99c7f35ef447b5db3e30f097c0c50859e195817d
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-01-01 11:03:31 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -1,12 +1,7 @@
 class Solution {
     public int solution(int a, int b, int n) {
-        //마트에 주는 병의 수 a
-        //빈 병 a를 가져다 주면 마트가 주는 콜라의 수 b
-        //내가 가진 빈 병의 수 n
-        //받을 수 있는 콜라의 병 수 return
         
         int answer = 0;
-        int emptyBottle = 0; 
         
         while(n>=a){
             answer += ((n/a) * b);

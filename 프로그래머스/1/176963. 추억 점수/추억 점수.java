126b1d5cfeba659c890bcfcd517b8944cd0fd8e8
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-02-13 14:55:11 +0900

    [level 1] Title: 추억 점수, Time: 2.22 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i<name.length; i++){
+            map.put(name[i], yearning[i]);
+        }
+        
+        // 1. 2중 for문
+        for(int i =0; i<photo.length; i++){
+            for(int j = 0; j<photo[i].length; j++){
+                if(map.get(photo[i][j]) != null){
+                    answer[i] += map.get(photo[i][j]);
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+}

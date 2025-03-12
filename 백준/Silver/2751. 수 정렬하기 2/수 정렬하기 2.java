9f7fa2d27c6f999d0a00ef13871c484673f236b5
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-03-12 23:47:46 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1448 ms, Memory: 169960 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int count = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for(int i =0; i<count; i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        Collections.sort(list);
+
+        for(int i =0; i<count; i++){
+            sb.append(list.get(i)).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}

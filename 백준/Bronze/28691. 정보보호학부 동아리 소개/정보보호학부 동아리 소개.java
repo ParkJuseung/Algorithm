4c9c4749c39ee02cc63a5b4b3d5978ac4838a3a9
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-03-16 23:44:42 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 160 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String next = scanner.next();
+
+        switch (next){
+            case "M" : System.out.println("MatKor"); break;
+            case "W" : System.out.println("WiCys"); break;
+            case "C" : System.out.println("CyKor");break;
+            case "A" : System.out.println("AlKor");break;
+            case "$" : System.out.println("$clear");break;
+        }
+
+        scanner.close();
+    }
+}

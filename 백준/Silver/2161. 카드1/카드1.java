7279a06ab9b1177c56cad944d0c6794321d992d8
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-03-23 23:40:06 +0900

    [Silver V] Title: 카드1, Time: 224 ms, Memory: 18668 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len  = scanner.nextInt();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        Queue<Integer> cards = new LinkedList<>();
+
+        for(int i=1; i<=len; i++){
+            cards.add(i);
+        }
+
+        while(cards.size() > 1) {
+            result.add(cards.poll());
+            cards.add(cards.poll());
+        }
+
+        for(int num : result){
+            System.out.print(num + " ");
+        }
+        System.out.println(cards.peek());
+    }
+}
+

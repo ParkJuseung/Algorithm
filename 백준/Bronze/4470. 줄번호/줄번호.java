f27a1f604a246f18fa5cd7ef8afd6949d721f746
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-05-08 23:50:11 +0900

    [Bronze IV] Title: 줄번호, Time: 196 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/4470. 줄번호/줄번호.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            String line = scanner.nextLine();
+            System.out.println(i + ". " + line);
+        }
+
+        scanner.close();
+    }
+}

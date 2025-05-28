b46476b0618c9210363218e144b913104b36bcf2
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-05-28 17:12:10 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 176 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for(int i = 1; i<=num; i++){
+            System.out.print(" ".repeat(num-i));
+            System.out.println("*".repeat(i));
+        }
+
+    }
+
+}

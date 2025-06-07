06a988c2bb3abd60d846d3938c2f33f33c07cd55
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-06-07 23:49:56 +0900

    [Bronze IV] Title: 세수정렬, Time: 196 ms, Memory: 18428 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(numbers);
+
+        for (int num : numbers) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}

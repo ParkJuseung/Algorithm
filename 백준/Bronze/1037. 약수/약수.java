80efa62581274e15483ddf9dca3b6fd2bdfd7f89
ParkJuseung <101164389+ParkJuseung@users.noreply.github.com>
2025-06-28 22:09:54 +0900

    [Bronze I] Title: 약수, Time: 180 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+        int[] divisors = new int[count];
+
+        for (int i = 0; i < count; i++) {
+            divisors[i] = sc.nextInt();
+        }
+
+        Arrays.sort(divisors);
+
+        int result = divisors[0] * divisors[count - 1];
+        System.out.println(result);
+    }
+}
